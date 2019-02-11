e86a31fc5de44d880b736da86901d0da3db21ff8
NicoFarmer <nicolas.santiago77.deal@gmail.com>
2019-02-11 01:42:33 +0100

    Suite de la mise en place du projet générique

--- Includes/CGlobale.h
+++ Includes/CGlobale.h
@@ -1,5 +1,5 @@
 /*! \file CGlobale.h
-	\brief Classe qui contient toute l'application
+        \brief Classe qui contient toute l'application
 */
 #ifndef _GLOBALE_H_
 #define _GLOBALE_H_
@@ -16,27 +16,27 @@
 #include "CMoteurs.h"
 #include "CAsservissementSulfateuse.h"
 #include "CAsservissementChariot.h"
-#include "MessengerXbeeNetwork2019.h"
+#include "MessengerXbeeNetwork.h"
 
 typedef enum {
-	MODE_AUTONOME = 0,
-	MODE_PILOTE_LABOTBOX,
-	MODE_PILOTE_TERMINAL
+    MODE_AUTONOME = 0,
+    MODE_PILOTE_LABOTBOX,
+    MODE_PILOTE_TERMINAL
 }tModeFonctionnement;
 
 // Pour le séquenceur de tâche
-#define PERIODE_TICK	(10)
-#define TEMPO_10msec	(10/PERIODE_TICK) 
-#define TEMPO_20msec	(20/PERIODE_TICK) 
-#define TEMPO_50msec	(50/PERIODE_TICK) 
-#define TEMPO_100msec	(100/PERIODE_TICK) 
-#define TEMPO_200msec	(200/PERIODE_TICK) 
-#define TEMPO_500msec	(500/PERIODE_TICK) 
-#define TEMPO_1sec		(1000/PERIODE_TICK) 
-#define TEMPO_2sec		(2000/PERIODE_TICK) 
-#define TEMPO_5sec		(5000/PERIODE_TICK) 
-#define TEMPO_10sec		(10000/PERIODE_TICK) 
-#define TEMPO_15sec		(15000/PERIODE_TICK) 
+#define PERIODE_TICK    (10)
+#define TEMPO_10msec    (10/PERIODE_TICK)
+#define TEMPO_20msec    (20/PERIODE_TICK)
+#define TEMPO_50msec    (50/PERIODE_TICK)
+#define TEMPO_100msec   (100/PERIODE_TICK)
+#define TEMPO_200msec   (200/PERIODE_TICK)
+#define TEMPO_500msec   (500/PERIODE_TICK)
+#define TEMPO_1sec      (1000/PERIODE_TICK)
+#define TEMPO_2sec      (2000/PERIODE_TICK)
+#define TEMPO_5sec      (5000/PERIODE_TICK)
+#define TEMPO_10sec     (10000/PERIODE_TICK)
+#define TEMPO_15sec     (15000/PERIODE_TICK)
 
 
 
@@ -44,92 +44,94 @@ typedef enum {
 //! Classe de gestion des options d'exécution passees en ligne de commande
 class CGlobale {
 public :
-	//! Le mode de fonctionnement (autonome ou piloté via Anaconbot)
-	unsigned int ModeFonctionnement;
+    //! Le mode de fonctionnement (autonome ou piloté via Anaconbot)
+    unsigned int ModeFonctionnement;
 
-	//le temps max de gonflage du ballon
-	unsigned int TempsMaxGonflage;
+    //le temps max de gonflage du ballon
+    unsigned int TempsMaxGonflage;
 
     //! Le match à  jouer
-	CMatch m_match;
-	//! La gestion des roues gauches et droites
-	CRoues m_roues;
- 	//! La gestion d'Anaconbot
-	CLaBotBox m_LaBotBox;
-	//! La gestion des capteurs
-	CCapteurs m_capteurs;
-	//! Le gestionnaire d'EEPROM
+    CMatch m_match;
+    //! La gestion des roues gauches et droites
+    CRoues m_roues;
+    //! La gestion d'Anaconbot
+    CLaBotBox m_LaBotBox;
+    //! La gestion des capteurs
+    CCapteurs m_capteurs;
+    //! Le gestionnaire d'EEPROM
     CEEPROM m_eeprom;
     //! La gestion des Led
     CLeds m_leds;
-	//! L'asservissement de vitesse/position du robot
+    //! L'asservissement de vitesse/position du robot
     // ATTENTION : l'instance de la classe asservisement doit être mise après l'instance de eeprom car CAsservissement utilise CEEPROM dans son constructeur
-	CAsservissement m_asservissement;
+    CAsservissement m_asservissement;
 
-	//! Gestion des servos moteurs controlés par le SD20
-	CServoMoteurSD20 m_servos_sd20;
-	//! Gestion des servos moteurs AX
-	CServoMoteurAX m_servos_ax;
- 	//! Gestion des moteurs
-	CMoteurs m_moteurs;
+    //! Gestion des servos moteurs controlés par le SD20
+    CServoMoteurSD20 m_servos_sd20;
+    //! Gestion des servos moteurs AX
+    CServoMoteurAX m_servos_ax;
+    //! Gestion des moteurs
+    CMoteurs m_moteurs;
 
- 	//! Gestion de l'asservissement de la sulfateuse
-	CAsservissementSulfateuse m_asservissement_sulfateuse;
-	
-	//! Gestion de l'asservissement du chariot
-	CAsservissementChariot m_asservissement_chariot;
+    //! Gestion de l'asservissement de la sulfateuse
+    CAsservissementSulfateuse m_asservissement_sulfateuse;
+
+    //! Gestion de l'asservissement du chariot
+    CAsservissementChariot m_asservissement_chariot;
 
     MessengerXbeeNetwork m_messenger_xbee_ntw;
 
     //debug
-	float m_x_debug, m_y_debug, m_teta_debug;
-
- 
-	CGlobale();
-	~CGlobale();
-
-	//! Point d'entree pour lancer l'application
-	void Run(void);
+    float m_x_debug, m_y_debug, m_teta_debug;
 
 
-	//!  Le programme d'IRQ qui gere le timer
-	void IRQ_Tick_ModeAutonome(void);
-	//!  Le programme d'IRQ qui gere le timer
-	void IRQ_Tick_ModePiloteLaBotBox(void);
-	//!  Le programme d'IRQ qui gere le timer
-	void IRQ_Tick_ModePiloteTerminal(void);
+    CGlobale();
+    ~CGlobale();
+
+    //! Point d'entree pour lancer l'application
+    void Run(void);
+
+
+    //!  Le programme d'IRQ qui gere le timer
+    void IRQ_Tick_ModeAutonome(void);
+    //!  Le programme d'IRQ qui gere le timer
+    void IRQ_Tick_ModePiloteLaBotBox(void);
+    //!  Le programme d'IRQ qui gere le timer
+    void IRQ_Tick_ModePiloteTerminal(void);
 
 
 private : 
-	//! Le tick du sequenceur de tache
-	unsigned int Tick;
-	
-	
-	//! Gestion du mode autonome
-	void ModeAutonome(void);
-	//! Sequenceur de taches en mode autonome
-	void SequenceurModeAutonome(void);
-	//! Sequenceur de taches en mode pilote par Anaconbot
-	void SequenceurModePiloteLaBotBox(void);
-	//! Sequenceur de taches en mode pilote par teminal RS232
-	void SequenceurModePiloteTerminal(void);
-	
-	
-	
-	//! Gestion du mode piloté via Anaconbot
-	void ModePiloteLaBotBox(void);
-	//! Reception RS232 en IRQ
+    //! Le tick du sequenceur de tache
+    unsigned int Tick;
+
+
+    //! Gestion du mode autonome
+    void ModeAutonome(void);
+    //! Sequenceur de taches en mode autonome
+    void SequenceurModeAutonome(void);
+    //! Sequenceur de taches en mode pilote par Anaconbot
+    void SequenceurModePiloteLaBotBox(void);
+    //! Sequenceur de taches en mode pilote par teminal RS232
+    void SequenceurModePiloteTerminal(void);
+
+
+
+    //! Gestion du mode piloté via Anaconbot
+    void ModePiloteLaBotBox(void);
+    //! Reception RS232 en IRQ
     //void ReceiveRS232_ModePiloteLaBotBox(void);
-	//! Verifie et traite la reception de trames d'Anaconbot
+    //! Verifie et traite la reception de trames d'Anaconbot
     //void CheckReceptionTrame(void);
-	//! Envoie les trames vers LABOTBOX
+    //! Envoie les trames vers LABOTBOX
     //void SendTramesLaBotBox(void);
 
-	//! Gestion du mode piloté par terminal
-	void ModePiloteTerminal(void);
- 	//! Reception RS232 en IRQ
-	void ReceiveRS232_ModePiloteTerminal(void);
+    //! Gestion du mode piloté par terminal
+    void ModePiloteTerminal(void);
+    //! Reception RS232 en IRQ
+    void ReceiveRS232_ModePiloteTerminal(void);
 
+    //! Lecture eeprom
+    void readEEPROM();
 };
 
 
--- Sources/CGlobale.cpp
+++ Sources/CGlobale.cpp
@@ -1,14 +1,12 @@
 /*! \file CGlobale.cpp
-	\brief Classe qui contient toute l'application
+    \brief Classe qui contient toute l'application
 */
 #include "mbed.h"
 #include "RessourcesHardware.h"
 #include "CGlobale.h"
 
-
-
 //___________________________________________________________________________
- /*!
+/*!
    \brief Constructeur
 
    \param --
@@ -16,11 +14,11 @@
 */
 CGlobale::CGlobale() 
 {
-  ModeFonctionnement = MODE_AUTONOME;
+    ModeFonctionnement = MODE_AUTONOME;
 }
 
 //___________________________________________________________________________
- /*!
+/*!
    \brief Destructeur
 
    \param --
@@ -32,7 +30,7 @@ CGlobale::~CGlobale()
 }
 
 //___________________________________________________________________________
- /*!
+/*!
    \brief Point d'entrée pour l'execution de toute l'application
 
    \param --
@@ -40,73 +38,96 @@ CGlobale::~CGlobale()
 */
 void CGlobale::Run(void)
 {
-  // Initialise la vitesse de transmission avec le PC
-  _rs232_pc_tx.baud(115200);
-  _rs232_pc_tx.format(8, Serial::None, 1);   	// 8 bits de données / Pas de parité / 1 bit de stop
-  _rs232_pc_rx.baud(115200);
-  _rs232_pc_rx.format(8, Serial::None, 1);   	// 8 bits de données / Pas de parité / 1 bit de stop
+    // Initialise la vitesse de transmission avec le PC
+    _rs232_pc_tx.baud(115200);
+    _rs232_pc_tx.format(8, Serial::None, 1);   	// 8 bits de données / Pas de parité / 1 bit de stop
+    _rs232_pc_rx.baud(115200);
+    _rs232_pc_rx.format(8, Serial::None, 1);   	// 8 bits de données / Pas de parité / 1 bit de stop
 
- _rs232_pc_tx.printf("\n\rHello\n\r");
+    _rs232_pc_tx.printf("\n\rHello\n\r");
 
-  //initialise la vitesse i2c
-  //_i2c.frequency(200000);
+    //initialise la vitesse i2c
+    //_i2c.frequency(200000);
 
-  // Attends la montée de toutes les alimentation et l'initialisation de l'écran
-  // Temps nécessaire en pratique pour que l'écran tactile ai fini de démarrer
-  // avant de commencer à  lui en envoyer des messages (et d'en recevoir) 
-  wait_ms(3000);
+    // Lecture des paramètres EEPROM
+    readEEPROM();
 
-  // Active les pull up sur les entrées
-  //_Etor1.mode(PullUp);
-  //_Etor2.mode(PullUp);
-  m_capteurs.Init();
-  /*if(m_capteurs.m_color_sensor_OK)
-	  _rs232_pc_tx.printf("\n\rCapteur couleur present\n\r");
-  else {
-	  _rs232_pc_tx.printf("\n\rPas de capteur couleur\n\r");
-}*/
-  //wait_ms(3000);
+    // Attends la montée de toutes les alimentation et l'initialisation de l'écran
+    // Temps nécessaire en pratique pour que l'écran tactile ai fini de démarrer
+    // avant de commencer à  lui en envoyer des messages (et d'en recevoir)
+    wait_ms(3000);
 
-  //Init de l'asservissement chariot
-  m_asservissement_chariot.Init();
+    // Active les pull up sur les entrées
+    //_Etor1.mode(PullUp);
+    //_Etor2.mode(PullUp);
+    m_capteurs.Init();
 
+    //Init de l'asservissement chariot
+    m_asservissement_chariot.Init();
 
- // Lecture des paramètres EEPROM et recopie dans les données membres 
- // de chaque classe en RAM
- m_eeprom.Read();
+    // Initialisation des servos moteurs
+    m_servos_sd20.Init();
+    m_servos_ax.Init();
 
- // Initialisation des servos moteurs
- m_servos_sd20.Init();
- m_servos_ax.Init();
- 
- //Init de l'asservissement chariot en cas de plantage de l'eeprom
- if ((Application.m_asservissement_chariot.commande_chariot_max_C==0) ||
-     (Application.m_asservissement_chariot.compensation_zone_morte_dw_C==0) ||
-     (Application.m_asservissement_chariot.compensation_zone_morte_up_C==0))
+    m_messenger_xbee_ntw.start();
+
+    //Init de l'asservissement chariot en cas de plantage de l'eeprom
+    if ((Application.m_asservissement_chariot.commande_chariot_max_C==0) ||
+            (Application.m_asservissement_chariot.compensation_zone_morte_dw_C==0) ||
+            (Application.m_asservissement_chariot.compensation_zone_morte_up_C==0))
         m_asservissement_chariot.Init();
 
- _rs232_pc_tx.printf("ki_angle = %f\n\r", Application.m_asservissement.ki_angle);
- _rs232_pc_tx.printf("cde_max = %d\n\r", Application.m_asservissement.cde_max);
- _rs232_pc_tx.printf("cde_min = %d\n\r", Application.m_asservissement.cde_min);
+    _rs232_pc_tx.printf("ki_angle = %f\n\r", Application.m_asservissement.ki_angle);
+    _rs232_pc_tx.printf("cde_max = %d\n\r", Application.m_asservissement.cde_max);
+    _rs232_pc_tx.printf("cde_min = %d\n\r", Application.m_asservissement.cde_min);
 
- /*_led1 = true;_led2 = false;_led3 = false;_led4 = false;wait(1);
- _led1 = false;_led2 = true;_led3 = false;_led4 = false;wait(1);
- _led1 = false;_led2 = false;_led3 = true;_led4 = false;wait(1);
- _led1 = false;_led2 = false;_led3 = false;_led4 = true;wait(1);
- _led1 = false;_led2 = false;_led3 = false;_led4 = false;wait(1);*/
+    switch(ModeFonctionnement) {
+    case MODE_AUTONOME : 			ModeAutonome();			break;
+    case MODE_PILOTE_LABOTBOX : 	ModePiloteLaBotBox();	break;
+    case MODE_PILOTE_TERMINAL : 	ModePiloteTerminal();	break;
+    default : 						ModeAutonome();			break;
+    }
 
- switch(ModeFonctionnement) {
-  	case MODE_AUTONOME : 			ModeAutonome();			break;
-	case MODE_PILOTE_LABOTBOX : 	ModePiloteLaBotBox();	break;
-	case MODE_PILOTE_TERMINAL : 	ModePiloteTerminal();	break;
-	default : 						ModeAutonome();			break;
- }
-
-  while(1) {
-		// Ce code ne devrait jamais être atteint
-  }
+    while(1) {
+        // Ce code ne devrait jamais être atteint
+    }
 }
 
+
+//___________________________________________________________________________
+/*!
+   \brief Lecture des paramètre de l'EEPROM
+
+   \param --
+   \return --
+*/
+void CGlobale::readEEPROM()
+{
+    m_eeprom.getValue("ModeFonctionnement", &ModeFonctionnement);
+
+    // valeurs pour le module Asservissement
+    m_eeprom.getValue("cde_max", &(m_asservissement.cde_max));
+    m_eeprom.getValue("cde_min", &(m_asservissement.cde_min));
+    m_eeprom.getValue("kp_distance", &(m_asservissement.kp_distance));
+    m_eeprom.getValue("ki_distance", &(m_asservissement.ki_distance));
+    m_eeprom.getValue("kp_angle", &(m_asservissement.kp_angle));
+    m_eeprom.getValue("ki_angle", &(m_asservissement.ki_angle));
+    m_eeprom.getValue("k_angle", &(m_asservissement.k_angle));
+    m_eeprom.getValue("seuil_conv_distance", &(m_asservissement.seuil_conv_distance));
+    m_eeprom.getValue("seuil_conv_angle", &(m_asservissement.seuil_conv_angle));
+    m_eeprom.getValue("compteur_max", &(m_asservissement.compteur_max));
+    m_eeprom.getValue("zone_morte_D", &(m_asservissement.zone_morte_D));
+    m_eeprom.getValue("zone_morte_G", &(m_asservissement.zone_morte_G));
+
+    m_eeprom.getValue("rackCommandeMax", &(m_asservissement_chariot.commande_chariot_max_C));
+    m_eeprom.getValue("rackGainPosVit", &(m_asservissement_chariot.gain_position_vitesse_C));
+    m_eeprom.getValue("rackGainP", &(m_asservissement_chariot.gain_prop_C));
+    m_eeprom.getValue("rackGainI", &(m_asservissement_chariot.gain_int_C));
+    m_eeprom.getValue("rackSeuilConv", &(m_asservissement_chariot.seuil_conv_C));
+    m_eeprom.getValue("rackZMdw", &(m_asservissement_chariot.compensation_zone_morte_dw_C));
+    m_eeprom.getValue("rackZMup", &(m_asservissement_chariot.compensation_zone_morte_up_C));
+    m_eeprom.getValue("rackGradVit", &(m_asservissement_chariot.offset_vitesse_max_C));
+}
 
 
 
