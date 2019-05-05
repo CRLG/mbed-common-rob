adeb441828cd15958a0effd13dc9db338b23cbb4
NicoFarmer <nicolas.santiago77.deal@gmail.com>
2019-05-06 00:05:05 +0200

    https://github.com/CRLG/GROSBOT_MBED/issues/25 Suppression des trace de debug du MessengerXbeeNetwork

--- Sources/MessengerXbeeNetwork.cpp
+++ Sources/MessengerXbeeNetwork.cpp
@@ -24,7 +24,6 @@ void MessengerXbeeNetwork::IRQ_ReceiveRS232()
 {
     char rxData;
     rxData = _rs232_xbee_network_rx.getc();
-    _rs232_pc_tx.putc(rxData);
     _led3 = !_led3;
     m_xbee.decode(rxData);
 }
